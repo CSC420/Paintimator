2a1d30544f0e862ac3345422df2214c829298c96
mwilli22 <mwilli22@oswego.edu>
2014-12-08 17:43:37 -0500

    I think I got it this time

--- Pantimator/src/pantimator/AnimationPane.java
+++ Pantimator/src/pantimator/AnimationPane.java
@@ -28,13 +28,14 @@ public class AnimationPane extends JPanel {
 	Image img;
 	LayeredPanelList lpl;
 	JScrollPane scrollframeHolder;
-    JPanel frameHolder;
+    JPanel frameHolder, btnHolder;
     Thumb thumbPanel;
     Paintimator painter;
-	JButton play;
+	JButton play, btn05Speed, btn10Speed, btn15Speed;
 	private Clip button;
     HashMap<Thumb, LayeredPanel> thumbMap;
     GridBagConstraints gbc = new GridBagConstraints();
+    int frameRate = 200;
 
 
     public AnimationPane(Paintimator painter) throws IOException {
@@ -74,6 +75,9 @@ public class AnimationPane extends JPanel {
         button = AudioSystem.getClip();
         button.open(ais);
         this.add(scrollframeHolder);
+        
+        btnHolder  = new JPanel(new GridBagLayout());
+        btnHolder.setOpaque(false);
         playButton();
         
     }
@@ -82,9 +86,79 @@ public class AnimationPane extends JPanel {
 
 		final java.net.URL imageURL = AnimationPane.class.getResource("images/play.png");
 		final java.net.URL imageGreenURL = AnimationPane.class.getResource("images/playGreen.png");
+		final java.net.URL image05URL = AnimationPane.class.getResource("images/05.png");
+		final java.net.URL image10URL = AnimationPane.class.getResource("images/10.png");
+		final java.net.URL image15URL = AnimationPane.class.getResource("images/15.png");
+		final java.net.URL image05GreenURL = AnimationPane.class.getResource("images/05Green.png");
+		final java.net.URL image10GreenURL = AnimationPane.class.getResource("images/10Green.png");
+		final java.net.URL image15GreenURL = AnimationPane.class.getResource("images/15Green.png");
 		  if (imageURL == null) {
 			 System.out.println("Issue loading play button in Animation Pane"); 
 		  }
+		  
+		  btn05Speed = new JButton(new ImageIcon(image05URL));
+		  btn05Speed.setBorder(BorderFactory.createEmptyBorder(1,1,1,1));
+		  btn05Speed.setOpaque(true);
+		  btn05Speed.setContentAreaFilled(false);
+		  btn05Speed.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					frameRate = 300;
+					btn05Speed.setIcon(new ImageIcon(image05GreenURL));
+					
+					if(btn10Speed != null){
+						btn10Speed.setIcon(new ImageIcon(image10URL));
+					}
+					
+					if(btn15Speed != null){
+						btn15Speed.setIcon(new ImageIcon(image15URL));
+					}
+					
+				}
+			});
+		  
+		  btn10Speed = new JButton(new ImageIcon(image10GreenURL));
+		  btn10Speed.setBorder(BorderFactory.createEmptyBorder(1,1,1,1));
+		  btn10Speed.setOpaque(true);
+		  btn10Speed.setContentAreaFilled(false);
+		  btn10Speed.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					frameRate = 200;
+					btn10Speed.setIcon(new ImageIcon(image10GreenURL));
+					
+					if(btn05Speed != null){
+						btn05Speed.setIcon(new ImageIcon(image05URL));
+					}
+					
+					if(btn15Speed != null){
+						btn15Speed.setIcon(new ImageIcon(image15URL));
+					}
+					
+				}
+			});
+		  
+		  btn15Speed = new JButton(new ImageIcon(image15URL));
+		  btn15Speed.setBorder(BorderFactory.createEmptyBorder(1,1,1,1));
+		  btn15Speed.setOpaque(true);
+		  btn15Speed.setContentAreaFilled(false);
+		  btn15Speed.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					frameRate = 100;
+					btn15Speed.setIcon(new ImageIcon(image15GreenURL));
+					
+					if(btn10Speed != null){
+						btn10Speed.setIcon(new ImageIcon(image10URL));
+					}
+					
+					if(btn05Speed != null){
+						btn05Speed.setIcon(new ImageIcon(image05URL));
+					}
+					
+				}
+		  });
+		  
 		 play = new JButton(new ImageIcon(imageURL));
 		 play.setBorder(BorderFactory.createEmptyBorder(1,1,1,1));
 		 play.setOpaque(true);
@@ -140,14 +214,44 @@ public class AnimationPane extends JPanel {
                 }
 
 
-                AnimationPlayerPanel app = new AnimationPlayerPanel(images.toArray(b));
+                AnimationPlayerPanel app = new AnimationPlayerPanel(images.toArray(b), frameRate);
 
                 JOptionPane.showOptionDialog(context, app, "Your Animation",
                         JOptionPane.NO_OPTION, JOptionPane.PLAIN_MESSAGE,
                         null, new Object[0], null);
 			}
 		});
-        this.add(play);
+
+		 gbc.gridx = 0;
+		 gbc.gridy = 0;
+		 gbc.gridheight = 1;
+		 gbc.gridwidth = 3;
+		 btnHolder.add(play, gbc);
+		
+		 gbc.gridx = 0;
+		 gbc.gridy = 1;
+		 gbc.gridheight = 1;
+		 gbc.gridwidth = 1;
+		 btnHolder.add(btn05Speed, gbc);
+		 
+		 
+		 gbc.gridx = 1;
+		 gbc.gridy = 1;
+		 gbc.gridheight = 1;
+		 gbc.gridwidth = 1;
+		 gbc.weightx = 2.4;
+		 btnHolder.add(btn10Speed, gbc);
+		 
+		 
+		 gbc.gridx = 2;
+		 gbc.gridy = 1;
+		 gbc.gridheight = 1;
+		 gbc.gridwidth = 1;
+		 gbc.weightx = 0.0;
+		 btnHolder.add(btn15Speed, gbc);
+		 
+		 this.add(btnHolder); 
+		 //this.add(play);
 	}
     
     /*
