3aa10a97dbfd57a981f0d14d471f636743bd1516
mwilli22 <mwilli22@oswego.edu>
2014-12-08 17:24:38 -0500

    I'm commiting every goddamn changes

--- Pantimator/src/pantimator/Paintimator.java
+++ Pantimator/src/pantimator/Paintimator.java
@@ -378,11 +378,11 @@ public class Paintimator extends JFrame{
 				JOptionPane.YES_NO_OPTION,
 				JOptionPane.QUESTION_MESSAGE,
 				null,
-				new String[]{"Add New Page", "No"},
+				new String[]{"Cancel", "Copy Page", "Blank Page"},
 				"Add New Page");
 		
 		switch (i) {
-			case JOptionPane.YES_OPTION :	
+			case 1 :	
 				centerPanel.remove(layeredPanelList.getSelected());
 				animationPane.updateAnimation(layeredPanelList, layeredPanelList.getIntSelectedPanel()+1);
 				
